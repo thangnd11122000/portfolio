19dea57ebcc45f9796101f69e6d9fe67c5288883
Nguyen Doan Thang <thangnd111220@gmail.com>
2022-03-01 13:48:35 +0700

    Push code

---
+++ src/components/NavigationDots.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const items = ["home", "about", "work", "skills", "testimonials", "contact"];
+
+const NavigationDots = ({ active }) => {
+  return (
+    <div className="app__navigation">
+      {items.map((item, index) => (
+        // eslint-disable-next-line jsx-a11y/anchor-has-content
+        <a
+          href={`#${item}`}
+          key={item + index}
+          className="app__navigation-dot"
+          style={active === item ? { backgroundColor: "#313BAC" } : {}}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default NavigationDots;
