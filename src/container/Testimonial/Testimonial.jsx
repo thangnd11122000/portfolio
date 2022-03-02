41fa01ff40a930cdb88e88cc8e31088cbe93503d
Nguyen Doan Thang <thangnd111220@gmail.com>
2022-03-02 20:00:21 +0700

    Remove form testumonial

--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -4,7 +4,6 @@ import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 import "./Testimonial.scss";
-import { useForm } from "react-hook-form";
 
 const Testimonial = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -21,25 +20,6 @@ const Testimonial = () => {
       setTestimonials(data);
     });
   }, []);
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-
-  const onSubmit = (data) => {
-    const demo = {
-      _type: "demo",
-      name: data.username,
-      company: data.company,
-      feedback: data.feedback,
-    };
-    client
-      .create(demo)
-      .then(() => alert("success"))
-      .catch((err) => console.log(err));
-  };
 
   return (
     <>
@@ -91,47 +71,6 @@ const Testimonial = () => {
           </div>
         </>
       )}
-
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="app__footer-form app__flex"
-      >
-        <div className="app__flex">
-          <input
-            className="p-text"
-            type="text"
-            placeholder="Your Name"
-            {...register("username", { required: true })}
-          />
-        </div>
-        <p className="app__footer-form--error">
-          {errors.username && <span>This name is required</span>}
-        </p>
-        <div className="app__flex">
-          <input
-            className="p-text"
-            type="text"
-            placeholder="Your company"
-            {...register("company", { required: true })}
-          />
-        </div>
-        <p className="app__footer-form--error">
-          {errors.company && <span>This company is required</span>}
-        </p>
-        <div>
-          <textarea
-            className="p-text"
-            placeholder="Your feedback"
-            {...register("feedback", { required: true })}
-          />
-        </div>
-        <p className="app__footer-form--error">
-          {errors.feedback && <span>This feedback is required</span>}
-        </p>
-        <button type="submit" className="p-text" onClick={handleSubmit}>
-          Send
-        </button>
-      </form>
     </>
   );
 };
